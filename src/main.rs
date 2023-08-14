b725915f09b4a683cf785c095dbe256bd09b6470
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-08-14 17:48:19 +0530

    Add main function

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,59 @@
-use std::ops::Deref;
 use std::time::Duration;
 
-const WIDTH: usize = 64;
-
-type ScreenBuff = [[char; WIDTH]; WIDTH];
-
-struct Buffer {
-    buffer: ScreenBuff,
-}
-
-impl Buffer {
-    fn init() -> Self {
-        Buffer {
-            buffer: [['-'; WIDTH]; WIDTH],
-        }
-    }
-}
-
-impl Deref for Buffer {
-    type Target = ScreenBuff;
-
-    fn deref(&self) -> &Self::Target {
-        &self.buffer
-    }
-}
-
-
+const WIDTH: i32 = 24;
 
 fn main() {
-    let mut buffer = Buffer::init();
+    let mut count = 0;
+    let mut inc_flag = true;
+    // Origin
+    // The origins affect their opposite axes
+    let (x_origin, y_origin) = ((WIDTH / 2) + 2, WIDTH / 2);
 
     loop {
         if !std::process::Command::new("clear").status().unwrap().success() { break }
-        buffer.iter().for_each(|row| {
-            row.iter().for_each(|col| {
-                print!("{}", col);
-            });
-            print!("\n");
-        });
 
-        std::thread::sleep(Duration::from_millis(24));
+        // Counting
+        if inc_flag {
+            count += 1;
+        } else {
+            count -= 1;
+        }
+        // This is for the max before we have to reverse the count
+        // Adjusts the bounds for radius expansion
+        if count >= WIDTH / 4 {
+            inc_flag = false;
+        }
+        if count <= 0 {
+            inc_flag = true;
+        }
+
+        // A
+        let radius1 = count;
+        // B
+        let radius2 = WIDTH / 4;
+
+        // Draw screen
+        for x_point in 0..WIDTH {
+            for y_point in 0..WIDTH {
+                let x_diff = x_point - x_origin;
+                let y_diff = y_point - y_origin;
+
+                let x_diff_sqr = x_diff * x_diff;
+                let y_diff_sqr = y_diff * y_diff;
+
+                let x_part = x_diff_sqr.checked_div(radius1.pow(2)).unwrap_or(2);
+                let y_part = y_diff_sqr.checked_div(radius2.pow(2)).unwrap_or(2);
+
+                if x_part + y_part > 1 {
+                    print!("-");
+                    continue
+                }
+
+                print!("#");
+            }
+            print!("\n");
+        }
+
+        std::thread::sleep(Duration::from_millis(84));
     }
 }
