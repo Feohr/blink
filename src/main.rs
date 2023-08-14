4a72c04dfc2267bf50fc1b80e1807a6d2ceb24a3
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-08-14 19:04:40 +0530

    Add Iris

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,88 @@
 use std::time::Duration;
 
-const WIDTH: i32 = 24;
+const WIDTH: f32 = 24_f32;
+const HEIGHT: f32 = WIDTH * 3_f32;
 
 fn main() {
-    let mut count = 0;
+    let mut count = 0_f32;
     let mut inc_flag = true;
     // Origin
     // The origins affect their opposite axes
-    let (x_origin, y_origin) = ((WIDTH / 2) + 2, WIDTH / 2);
+    let (x_origin, y_origin) = ((WIDTH / 2_f32) + 2_f32, HEIGHT / 2_f32);
+
+    // Minor axis
+    let radius2 = (HEIGHT / 2_f32) as f32;
+
+    // Pupil
+    let radius3 = 8_f32;
+    let (x_iris_origin, y_iris_origin) = (x_origin, y_origin);
 
     loop {
-        if !std::process::Command::new("clear").status().unwrap().success() { break }
+        std::process::Command::new("clear").status().unwrap();
 
         // Counting
         if inc_flag {
-            count += 1;
+            count += 0.5_f32;
         } else {
-            count -= 1;
+            count -= 0.5_f32;
         }
+
         // This is for the max before we have to reverse the count
         // Adjusts the bounds for radius expansion
-        if count >= WIDTH / 4 {
+        if count >= WIDTH / 2.7_f32 {
             inc_flag = false;
         }
-        if count <= 0 {
+        if count <= 0_f32 {
             inc_flag = true;
         }
 
-        // A
-        let radius1 = count;
-        // B
-        let radius2 = WIDTH / 4;
+        // Major axis
+        let radius1 = count as f32;
 
-        // Draw screen
-        for x_point in 0..WIDTH {
-            for y_point in 0..WIDTH {
+       // Draw screen
+        for x_point in 0..(WIDTH as usize) {
+            let mut is_next = true;
+
+            for y_point in 0..(HEIGHT as usize) {
+                let x_point = x_point as f32;
+                let y_point = y_point as f32;
+
+                if (x_origin - 2_f32..x_origin + 3_f32).contains(&x_point) {
+                    is_next = false;
+                    break
+                }
+
+                // For circle
+                {
+                    let x_diff = x_point - x_iris_origin;
+                    let y_diff = y_point - y_iris_origin;
+                    let x_diff_sqr = x_diff * x_diff;
+                    let y_diff_sqr = y_diff * y_diff;
+
+                    if x_diff_sqr + y_diff_sqr < radius3.powi(2) {
+                        print!(" ");
+                        continue
+                    }
+                }
+
                 let x_diff = x_point - x_origin;
                 let y_diff = y_point - y_origin;
-
                 let x_diff_sqr = x_diff * x_diff;
                 let y_diff_sqr = y_diff * y_diff;
+                let x_part = x_diff_sqr / radius1.powi(2);
+                let y_part = y_diff_sqr / radius2.powi(2);
 
-                let x_part = x_diff_sqr.checked_div(radius1.pow(2)).unwrap_or(2);
-                let y_part = y_diff_sqr.checked_div(radius2.pow(2)).unwrap_or(2);
-
-                if x_part + y_part > 1 {
-                    print!("-");
+                if x_part + y_part < 1_f32 {
+                    print!("█");
                     continue
                 }
 
-                print!("#");
+                print!(" ");
             }
-            print!("\n");
+
+            if is_next { print!("\n") }
         }
 
-        std::thread::sleep(Duration::from_millis(84));
+        std::thread::sleep(Duration::from_millis(42));
     }
 }
